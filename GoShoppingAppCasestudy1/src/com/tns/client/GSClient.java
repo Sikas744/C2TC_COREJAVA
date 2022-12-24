7b17114adac7ecf7628c44bf9800476675e8d431
sikas <sikas@SikasReddy>
2022-12-24 11:58:49 +0530

    name

--- GoShoppingAppCasestudy1/src/com/tns/client/GSClient.java
+++ GoShoppingAppCasestudy1/src/com/tns/client/GSClient.java
@@ -8,7 +8,7 @@ public class GSClient {
 
 	public static void main(String[] args) {
 		GSShopFactory factory=new GSShopFactory();
-		GSNormalAcc gsn=(GSNormalAcc)factory.getNewNormalAccount(1212, "padmini", 600.00f);
+		GSNormalAcc gsn=(GSNormalAcc)factory.getNewNormalAccount(1212, "Bhuvi", 600.00f);
 		gsn.bookProduct(gsn.getCharges());
 		GSPrimeACC gsp=(GSPrimeACC) factory.getNewPrimeAccount(12122, "suresh", 500.00f, true);
 		gsp.bookProduct(gsp.getCharges());
